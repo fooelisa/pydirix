e811e4d935a091e07de07f305d8f495339ec112e
Elisa Jasinska <elisa@jasinska.de>
2015-09-23 13:55:26 +0200

    adding method list_members_by_asn

--- pydirIX/dirIX.py
+++ pydirIX/dirIX.py
@@ -36,44 +36,75 @@ class dirIX:
         """
         List members at IXP
         """
-        # init result dict
+        # init result list
         result = []
+        # collect data 
         for member in self.data['member_list']:
             result.append(member['name'])
+        # return result list
         return result
 
     def list_asn(self):
         """
         List ASNs at IXP
         """
-        # init result dict
+        # init result list
         result = []
+        # collect data 
         for member in self.data['member_list']:
             result.append(member['asnum'])
+        # return result list
         return result
 
     def list_ip(self):
         """
         List IPs at IXP
         """
-        # init result dict
+        # init result list
         result = []
+        # collect data 
         for member in self.data['member_list']:
             for connection in member['connection_list']:
                  for vlan in connection['vlan_list']:
                     if 'ipv4' in vlan:
                         result.append(vlan['ipv4']['address'])
+        # return result list
         return result
 
     def list_ipv6(self):
         """
         List IPs at IXP
         """
-        # init result dict
+        # init result list
         result = []
+        # collect data 
         for member in self.data['member_list']:
             for connection in member['connection_list']:
                  for vlan in connection['vlan_list']:
                     if 'ipv6' in vlan:
                         result.append(vlan['ipv6']['address'])
+        # return result list
         return result
+
+    def list_members_by_asn(self):
+        """
+        Show member data by ASN
+        """
+        # init result dict
+        result = {}
+        # collect data 
+        for member in self.data['member_list']:
+            asn = member['asnum']
+            result[asn] = {}
+            result[asn]['name'] = member['name']
+            result[asn]['ip'] = []
+            result[asn]['ipv6'] = []
+            for connection in member['connection_list']:
+                 for vlan in connection['vlan_list']:
+                    if 'ipv4' in vlan:
+                        result[asn]['ip'].append(vlan['ipv4']['address'])
+                    if 'ipv6' in vlan:
+                        result[asn]['ipv6'].append(vlan['ipv6']['address'])
+        # return result dict
+        return result
+
