36dfa1c6d8c80307eb7dfe9140c8b11fad7b906a
Elisa Jasinska <elisa@jasinska.de>
2015-09-23 10:21:34 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # pydirix
 A Python parser to the Euro-IX JSON schema for IXP member lists
+
+Install
+=======
+
+To install, execute:
+
+```
+pip install pydirix
+```
+
+Documentation
+=============
+
+### Init Member Lists
+Get IXP member lists:
+```python
+>>> from pydirIX import dirIX
+>>> members = dirIX(urls='...')
+>>> members.get()
+```
+
+### Get AS Numbers
+call asns():
+```python
+>>> members.asns()
+>>>
+```
+
+License
+======
+
+Copyright 2015 BigWaveIT.
+
+Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
---
+++ setup.py
@@ -0,0 +1,42 @@
+# Copyright 2015 BigWaveIT. All rights reserved.
+#
+# The contents of this file are licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+import uuid
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
+
+# reqs is a list of requirement
+# e.g. ['django==1.5.1', 'mezzanine==1.4.6']
+reqs = [str(ir.req) for ir in install_reqs]
+
+version = '0.1'
+
+setup(
+    name='pydirIX',
+    version=version,
+    py_modules=['pydirIX'],
+    packages=find_packages(),
+    install_requires=reqs,
+    include_package_data=True,
+    description = 'A Python parser to the Euro-IX JSON schema for IXP member lists',
+    author = 'Elisa Jasinska',
+    author_email = 'elisa@bigwaveit.org',
+    url = 'https://github.com/fooelisa/pydirix/', # use the URL to the github repo
+    download_url = 'https://github.com/fooelisa/pydirix/tarball/%s' % version,
+    keywords = ['IXP', 'Euro-IX', 'networking'],
+    classifiers = [],
+)
